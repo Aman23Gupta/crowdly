1d83d9a22595d545212d3066c1451b502db41a95
Aman23Gupta <amang234284@gmail.com>
2020-02-09 16:16:50 +0530

    graphql mutation

--- server/src/graphql/resolvers/index.js
+++ server/src/graphql/resolvers/index.js
@@ -2,6 +2,12 @@ import tweetResolvers from "./tweet-resolvers";
 
 export default {
   Query: {
+    getTweet: tweetResolvers.getTweet,
     getTweets: tweetResolvers.getTweets
+  },
+  Mutation: {
+    createTweet: tweetResolvers.createTweet,
+    updateTweet: tweetResolvers.updateTweet,
+    deleteTweet: tweetResolvers.deleteTweet,
   }
 };
--- server/src/graphql/schema.js
+++ server/src/graphql/schema.js
@@ -1,16 +1,31 @@
 import { gql } from "apollo-server-express";
 
 export default gql`
+  scalar Date
+  type Status {
+    message: String!
+  }
+
   type Tweet {
-    _id: String
-    text: String
+    _id: ID!
+    text: String!
+    createdAt: Date!
+    updatedAt: Date!
   }
 
   type Query {
+    getTweet(_id: ID!): Tweet
     getTweets: [Tweet]
+  }
+
+  type Mutation {
+    createTweet(text: String!): Tweet
+    updateTweet(_id: ID!, text: String): Tweet
+    deleteTweet(_id: ID!): Status
   }
 
   schema {
     query: Query
+    mutation: Mutation
   }
 `;
